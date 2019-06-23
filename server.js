0eaf89d73de90bb3a2ccb0390b90753b5c0d8294
Mike <mjeanjacques90@gmail.com>
2019-06-23 14:31:08 -0400

    set up server.js file

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = requre("express");
+
+var app = express();
+
+var PORT = process.env.PORT || 1738;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+require("./app/routing/apiroutes")(app);
+require("./app/routing/htmlroutes")(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
