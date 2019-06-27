c11fb1ad09e0b53bb0eaf464a9c75c5097a8a3ff
Mike <mjeanjacques90@gmail.com>
2019-06-26 20:23:44 -0400

    reload page on submit and check for best friend

--- app/routing/apiroutes.js
+++ app/routing/apiroutes.js
@@ -7,12 +7,39 @@ module.exports = function(app) {
     });
 
     app.post("/api/friends", function(req, res){
+
+        var bestMatch = {
+            name: "",
+            photo: "",
+            difference: 1000
+        };
+
         var newFreind = req.body;
-        newFreind.routeName = newFreind.name.replace(/\s+/g, "").toLowerCase();
+        var newScores = newFreind.scores;
+
+        var totalDiff = 0;
+
+        for (i = 0; i < friendList.length; i++){
+            console.log(friendList[i]);
+            totalDiff = 0;
+            for(j = 0; j < friendList[i].scores[j]; j++){
+                totalDiff += Math.abs(parseInt(newScores[j]) - parseInt(friendList[i].scores[j]) );
+
+                if (totalDiff <= bestMatch.difference){
+                    bestMatch.name = friendList[i].name;
+                    bestMatch.photo = friendList[i].photo;
+                    bestMatch.difference = totalDiff;
+                };
+            };
+        };
+        // newFreind.routeName = newFreind.name.replace(/\s+/g, "").toLowerCase();
 
         console.log(newFreind);
-        res.json(newFreind);
+        console.log(bestMatch);
+        // console.log(newScores);
+        // res.json(newFreind);
         friendList.push(newFreind);
+        res.json(bestMatch);
 
     });
 
